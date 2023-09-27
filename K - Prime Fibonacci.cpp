5548b92c9b0eef08d78aae987e782d9c971e6313
Md. Maksudur Rahman <130817141+ginSpirit@users.noreply.github.com>
2023-09-27 14:53:26 +0600

    Update K - Prime Fibonacci.cpp
    Assiut University Training - Newcomers prime fibonacci

--- K - Prime Fibonacci.cpp
+++ K - Prime Fibonacci.cpp
@@ -1,24 +1,6 @@
 #include <bits/stdc++.h>
-#include <initializer_list>
-#include <scoped_allocator>
-#include <system_error>
-#include <type_traits>
-#include <unordered_map>
-#include <unordered_set> 
 using namespace std;
-typedef long long ll;
-typedef long long int int64;
-#define input(v) for(auto &i : v) cin >> i
-#define line(x) getline(cin, x)
-#define endl "\n"
-#define exit exit(EXIT_SUCCESS);
-#define revArray(x, n) reverse(x, x+n);
-#define all(x)  x.begin(), x.end()
 
-string spaceRemove(string &s) {
-    s.erase(remove(s.begin(), s.end(), ' '), s.end());
-    return s;
-}
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
     long long n;
